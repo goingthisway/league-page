3692ea9029a48045cf8d9072b288e4847a7489f4
goingthisway <maxsalem4@gmail.com>
2022-09-13 21:06:03 -0700

    fixed syntax error

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -122,7 +122,7 @@ export const managers = [
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Aidan Van Gieson", // Can be anything (usually your rival's name)
-        link: ,5 // manager array number within this array, or null to link back to all managers page
+        link: 5, // manager array number within this array, or null to link back to all managers page
         image: "/managers/everyone.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 4314, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
