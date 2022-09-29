0174970ba70fb446762353fe26ed0690eb0c3499
goingthisway <maxsalem4@gmail.com>
2022-09-29 16:39:59 -0700

    testing table in dropdown

--- src/routes/powerrankings/index.svelte
+++ src/routes/powerrankings/index.svelte
@@ -108,7 +108,26 @@
 
 <div>
     <select onchange="document.getElementById('seasonContent').innerHTML=options[selectedIndex].value;">
-        <option value="2022">2022</option>
+        <option value="2022">
+            <table>
+              <tr>
+                <th>Link</th>
+                <th>Author</th>
+              </tr>
+              <tr>
+                <td><a href="https://drive.google.com/file/d/1KcHl-yplK6Xj6YHD0sXdaVwMw_xao6C5/view?usp=sharing">Week 1 League Report</a></td>
+                <td>Chern</td>
+              </tr>
+              <tr>
+                <td><a href="https://drive.google.com/file/d/1OuxL9bX7-vBGs2bbZjm_rAfXmp4AZX2L/view?usp=sharing">Week 2 League Report</a></td>
+                <td>Max</td>
+              </tr>
+              <tr>
+                <td><a href="https://drive.google.com/file/d/18dG0v5h3Pjgi8hIlCqyBSr70GrGHnSjV/view?usp=sharing">Week 3 League Report</a></td>
+                <td>Luo</td>
+              </tr>
+            </table>
+        </option>
         <option value="2021">2021</option>
     </select>
 </div>
