4a4e5f576a390f46f88bafdcbe994405644346b9
goingthisway <maxsalem4@gmail.com>
2023-09-12 15:23:31 -0700

    random try

--- src/routes/powerrankings/index.svelte
+++ src/routes/powerrankings/index.svelte
@@ -118,25 +118,19 @@
         twenty_two = document.getElementById("twentyTwoTable");
         twenty_one = document.getElementById("twentyOneTable");
         if(view1 == "2023"){
-            twenty_one.after(twenty_two);
-            twenty_three.before(twenty_two);
-            twenty_three.before(twenty_one);
+            twenty_three.before(twenty_two, twenty_one);
             twenty_three.style.visibility = 'visible';
             twenty_two.style.visibility = 'hidden';
             twenty_one.style.visibility = 'hidden';
         }
         else if(view1 == "2022"){
-            twenty_one.after(twenty_three);
-            twenty_two.before(twenty_three);
-            twenty_two.before(twenty_one);
+            twenty_two.before(twenty_three, twenty_one);
             twenty_three.style.visibility = 'hidden';
             twenty_two.style.visibility = 'visible';
             twenty_one.style.visibility = 'hidden';
         }
         else{
-            twenty_two.after(twenty_three);
-            twenty_one.before(twenty_three);
-            twenty_one.before(twenty_two);
+            twenty_one.before(twenty_three, twenty_two);
             twenty_three.style.visibility = 'hidden';
             twenty_two.style.visibility = 'hidden';
             twenty_one.style.visibility = 'visible';
