2b17bda3bb4835da329d816849ffd00874e69bec
goingthisway <maxsalem4@gmail.com>
2023-09-15 00:40:04 -0700

    updated top tab name

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -29,7 +29,7 @@
 </script>
 
 <svelte:head>
-	<title>{!$page.url.pathname[1] ? 'Home' : $page.url.pathname[1].toUpperCase() + $page.url.pathname.slice(2)} | League Page</title>
+	<title>{!$page.url.pathname[1] ? 'Home' : $page.url.pathname[1].toUpperCase() + $page.url.pathname.slice(2)} | League of Horses</title>
 </svelte:head>
 
 <style>
