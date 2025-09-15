79ec334e52648c468e78bb6bd0582dd121e34511
goingthisway <maxsalem4@gmail.com>
2025-09-14 23:49:15 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1124813474044588032"; // your league ID
+export const leagueID = "1257059112693272576"; // your league ID
 export const leagueName = "League Of Horses"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
